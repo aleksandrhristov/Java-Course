5ab39860af854ee37c3289689a862c24eed090b3
Martin Milev <martinmariusmilev@gmail.com>
2016-04-22 17:21:25 +0300

    1

---
+++ IO/src/ex6/DataClass.java
@@ -0,0 +1,19 @@
+package ex6;
+
+import java.io.*;
+
+/**
+ * Created by clouway on 22.04.16.
+ */
+public class DataClass implements Serializable{
+
+  public void saveObject(OutputStream out, Object o){
+
+  }
+
+  public void getObject(InputStream in){
+
+  }
+
+
+}
---
+++ IO/src/ex6/Demo.java
@@ -0,0 +1,11 @@
+package ex6;
+
+
+/**
+ * Created by clouway on 22.04.16.
+ */
+public class Demo {
+  public static void main(String[] args) {
+
+  }
+}
