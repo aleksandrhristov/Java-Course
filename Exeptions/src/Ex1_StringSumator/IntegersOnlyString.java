0c5b2a3b6d570ba5e5245d51a2333c5d1fb286dc
ivanP91 <ivan.peichev91@gmail.com>
2016-04-25 14:18:13 +0300

    dfs

--- Exeptions/src/Ex1_StringSumator/IntegersOnlyString.java
+++ Exeptions/src/Ex1_StringSumator/IntegersOnlyString.java
@@ -1,8 +1,5 @@
 package Ex1_StringSumator;
 
-/**
- * Created by clouway on 15.04.16.
- */
 public class IntegersOnlyString extends Exception {
   public IntegersOnlyString() {
     super("The value must be an Integer.");
