c8a710bb3f9685c36a011ace08f51e9049b8daa0
Aleksandar Hristov <alexanderhristov93@abv.bg>
2016-04-26 13:20:55 +0300

    ADDED NEW TWO ELEMENTS TO THE LIST

--- Exeptions/src/list/DemoEx3.java
+++ Exeptions/src/list/DemoEx3.java
@@ -5,12 +5,14 @@ package list;
  */
 public class DemoEx3 {
   public static void main(String[] args) {
-    List l1 = new List(5);
+    List l1 = new List(7);
     l1.add("sasa");
     l1.add("123");
     l1.add("EWQEW");
     l1.add("124");
     l1.add("126");
+    l1.add("OBJJJJJJ");
+    l1.add("xD");
     System.out.println(l1.printAllElements());
     l1.remove("EWQEW");
     System.out.println(l1.printAllElements());
